ca84df017463c99d3f025249c53514b5cbd84212
danielspatacean <djdanydk@yahoo.com>
2016-12-01 23:14:03 +0200

    Refactor LoginActivity -> Clean code

--- app/src/main/java/appmobile/employeemanagerapp/activities/LoginActivity.java
+++ app/src/main/java/appmobile/employeemanagerapp/activities/LoginActivity.java
@@ -1,26 +1,13 @@
 package appmobile.employeemanagerapp.activities;
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.graphics.Path;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
-
 import appmobile.employeemanagerapp.R;
 import appmobile.employeemanagerapp.tasks.LoginAsync;
 import appmobile.employeemanagerapp.utils.Validator;
@@ -30,8 +17,8 @@ public class LoginActivity extends Activity {
     private EditText editTextPassword;
     private Validator validator;
 
-    String username;
-    String password;
+    private String username;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
